2060294710a3799f3d8e8d514cca9c7bffc26c95
Alessio Forte <AlessioF@macbook-air-di-alessio.local>
2016-07-11 11:52:34 +0200

    Update Project

--- HearthGuide/BannerController.swift
+++ HearthGuide/BannerController.swift
@@ -1,8 +1,8 @@
 //
-//  BannerController.swift
+//  BannerViewController.swift
 //  HearthGuide
 //
-//  Created by Alessio Forte on 05/07/16.
+//  Created by Alessio Forte on 11/07/16.
 //  Copyright © 2016 Alessio Forte. All rights reserved.
 //
 
@@ -11,14 +11,16 @@ import GoogleMobileAds
 
 class BannerController: UIViewController, GADBannerViewDelegate {
   
+  @IBOutlet var lockBorder: UIImageView!
+  @IBOutlet var topLock: UIImageView!
+  @IBOutlet var bottomLock: UIImageView!
+  @IBOutlet var vwBanner: GADBannerView!
   @IBOutlet var vwContainer: UIView!
   @IBOutlet var imgBack: UIImageView!
-  @IBOutlet var vwBanner: GADBannerView!
   @IBOutlet var heightCon: NSLayoutConstraint!
   @IBOutlet var topCon: NSLayoutConstraint!
-
+  
   override func viewDidLoad() {
-    
     super.viewDidLoad()
     
     vwBanner.delegate           = self
@@ -32,7 +34,12 @@ class BannerController: UIViewController, GADBannerViewDelegate {
     request.testDevices         = [kGADSimulatorID]
     
     vwBanner.loadRequest(request)
-
+  }
+  
+  override func viewDidAppear(animated: Bool) {
+    super.viewDidAppear(animated)
+    
+    openLock()
   }
   
   func adView(bannerView: GADBannerView!, didFailToReceiveAdWithError error: GADRequestError!) {
@@ -47,18 +54,39 @@ class BannerController: UIViewController, GADBannerViewDelegate {
   
   func adViewDidReceiveAd(bannerView: GADBannerView!) {
     self.topCon.constant    = 50
-
+    
     UIView.animateWithDuration(0.5) {
       self.view.layoutIfNeeded()
-    }  }
+    }
+  }
   
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  func openLock() {
+    
+    UIView.animateWithDuration(1, delay: 0, options: [], animations: {
+      
+      // Open lock.
+      let yDelta = self.lockBorder.frame.maxY
+      
+      print(yDelta)
+      
+      self.topLock.center.y    -= yDelta
+      self.lockBorder.center.y -= yDelta
+      self.bottomLock.center.y += yDelta
+      }, completion: { _ in
+        self.topLock.removeFromSuperview()
+        self.lockBorder.removeFromSuperview()
+        self.bottomLock.removeFromSuperview()
+    })
   }
   
   //Serve per impostare la Status Bar Bianca
   override func preferredStatusBarStyle() -> UIStatusBarStyle {
     return UIStatusBarStyle.LightContent
   }
-
+  
 }
